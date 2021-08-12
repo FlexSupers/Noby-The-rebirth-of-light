7128116609f02c69af2bdc0132943619534b9ad1
FlexSupers <flexsupers@gmail.com>
2021-08-13 01:09:08 +0700

    .

---
+++ Tests/Enemy test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class EnemyTests
+{
+    [Test]
+    public void Enemy_agroridle()
+    {
+        //arrange
+        var enemy = new Enemy;
+        //act
+        (distance > 15)
+       //assert
+       Assert.AreEqual(Enemy.idle("stay"))
+
+    }
+    {
+        //arrange
+        var enemy = new Enemy
+        //act
+        (distance <= 15 & distance > 1.5f)
+        //assert
+        Assert.AreEqual(Enemy.walk)
+}
+{
+    //arrange
+    var enemy = new Enemy
+    //act
+    (distance <= 1.5f)
+    //assert
+    Assert.AreEqual(enemy.Attack)
+}
+}
